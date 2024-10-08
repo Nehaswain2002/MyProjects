bb55e7687ce511628de0a28a55c26ad3f22ee2a4
Neha Swain <nehaswain7@gmail.com>
2024-10-09 00:45:57 +0530

    updated

---
+++ SPOTIFY CLONE/index.js
@@ -0,0 +1,17 @@
+// console.log("Welcome to Spotify");
+// let songIndex=0;
+
+// let songs =[
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+//     {songName:"salame-e-ishq",filepath:"song/1.mp3",coverpath:"cover/1.jpg"},
+// ]
+// let audioElement = new Audio('./1.mp3');
+// //  audioElement,play();
