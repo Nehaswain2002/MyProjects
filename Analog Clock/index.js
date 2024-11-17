d7ea97944005d6d4ec3f1bf409bb6363ed2057be
Neha Swain <nehaswain7@gmail.com>
2024-11-17 09:36:19 +0530

    updated

---
+++ Analog Clock/index.js
@@ -0,0 +1,44 @@
+// Analog clock
+let hr = document.querySelector("#hr");
+let min = document.querySelector("#min");
+let sc = document.querySelector("#sc");
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotatez(${hh + mm / 12}deg)`;
+  min.style.transform = `rotatez(${mm}deg)`;
+  sc.style.transform = `rotatez(${ss}deg)`;
+}, 1000);
+
+// Digital clock
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let ampm = document.getElementById("ampm");
+
+setInterval(() => {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  let am = h >= 12 ? "PM" : "AM";
+
+  // Convert 24-hour clock to 12-hour clock
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  // Add zero before single-digit numbers
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
+}, 1000);
